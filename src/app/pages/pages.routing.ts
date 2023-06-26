da824a4e282560cb29a7f060123b2c6c9e38250c
“Brayan <bretamozos24@gmail.com>
2023-06-25 19:18:59 -0500

    Finish router childs

---
+++ src/app/pages/pages.routing.ts
@@ -0,0 +1,39 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgessComponent } from './progess/progess.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
+
+const routes: Routes = [
+    {
+        path: 'dashboard',
+        component: PagesComponent,
+        children: [
+          {
+            path: '',
+            component: DashboardComponent
+          },
+          {
+            path: 'progress',
+            component: ProgessComponent
+          },
+          {
+            path: 'grafica1',
+            component: Grafica1Component
+          },
+        ]
+    },
+];
+
+@NgModule({
+    declarations: [],
+    imports: [
+      RouterModule.forChild(routes)
+    ],
+    exports: [
+      RouterModule
+    ]
+})
+export class PagesRoutingModule {}
